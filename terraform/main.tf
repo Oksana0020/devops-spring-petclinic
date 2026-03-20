3927b5756ce9b82ac039d182caf40dbe420dd7c4
Oksana <oksana.fursa@students.ittralee.ie>
2026-03-20 05:28:19 +0000

    Provision AWS EC2 infrastructure with security group using Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_security_group" "petclinic_sg" {
+  name        = "petclinic-sg"
+  description = "Security group for PetClinic EC2 instance"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["193.203.139.111/32"]
+  }
+
+  ingress {
+    description = "Application Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "petclinic-sg"
+  }
+}
+
+resource "aws_instance" "petclinic_ec2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  key_name               = "petclinic-key"
+  vpc_security_group_ids = [aws_security_group.petclinic_sg.id]
+
+  tags = {
+    Name = "petclinic-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.petclinic_ec2.public_ip
+}   
